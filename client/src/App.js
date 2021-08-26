88511f6dea9aafe2d64df0e80f9da93c4b2a6bc7
Sucho34 <suchomelpayton@gmail.com>
2021-08-25 21:27:15 -0400

    Fully Functional. Senta to DB from front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,68 @@
-
+import { useState } from "react";
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [position, setPosition] = useState("");
+  const [country, setCountry] = useState("");
+  const [salary, setSalary] = useState(0);
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      position: position,
+      country: country,
+      salary: salary
+    }).then(() => {
+      console.log("success");
+    })
+  };
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input 
+          type="text" 
+          onChange={(event) => 
+          {setName(event.target.value)
+          }}
+        />
+        <label>Age:</label>
+        <input 
+          type="number" 
+          onChange={(event) => 
+          {setAge(event.target.value)
+          }}
+        />
+        <label>Position:</label>
+        <input 
+          type="text" 
+          onChange={(event) => 
+          {setPosition(event.target.value)
+          }}
+        />
+        <label>Country:</label>
+        <input 
+          type="text" 
+          onChange={(event) => 
+          {setCountry(event.target.value)
+          }}
+        />
+        <label>Salary:</label>
+        <input 
+          type="number" 
+          onChange={(event) => 
+          {setSalary(event.target.value)
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+    </div>
   );
 }
 
